fc51c168adda52ba81fc6fd6c83c88d955f06365
Eric Yuen <eric.yuen@hellofresh.ca>
2022-12-21 15:43:11 -0500

    Change useTheme to take in styleSheetFn

--- useTheme.ts
+++ useTheme.ts
@@ -1,6 +1,8 @@
-import { useTheme as StyledComponentsUseTheme } from 'styled-components';
+import { useContext } from 'react';
 import { ThemeSpecification } from './specification';
+import { ThemeProviderContext } from './ThemeProvider';
 
-export default function useTheme(): Readonly<ThemeSpecification> {
-  return StyledComponentsUseTheme();
+export default function useTheme(styleSheetFn: (params: ThemeSpecification) => any): Readonly<ThemeSpecification> {
+  const theme = useContext(ThemeProviderContext);
+  return styleSheetFn(theme);
 }

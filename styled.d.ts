3e5238bc0818714f5e8d41eeaf0e8380a82363c0
Eric Yuen <eric.yuen@hellofresh.ca>
2022-12-23 18:45:02 -0500

    Remove 'styled-components'

--- styled.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components';
-import { ThemeSpecification } from './specification';
-
-declare module 'styled-components' {
-  // https://styled-components.com/docs/api#create-a-declarations-file
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends ThemeSpecification {}
-}
